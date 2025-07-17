c5c576cf991e13cfeea69603fcf1f5e96c01de70
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-07-17 12:36:41 +0300

    Commit

--- src/Rock_Paper_Scissors/Main.java
+++ src/Rock_Paper_Scissors/Main.java
@@ -1,7 +1,34 @@
 package Rock_Paper_Scissors;
 
-public class Main {
-    public static void main(Integer[] args) {
+import java.util.Scanner; //test
+import java.util.Random;
 
+public class Main {
+    public static void main(String[] args) {
+        int vasya = choose("Вася");
+        int petya = choose("Петя");
+
+        if (vasya != petya) {
+            int[] result = {vasya, petya};
+            if (!(result[0] + result[1] == 2 && Math.abs(result[0] - result[1]) == 2)) {
+
+            }
+        } else {
+            System.out.print("Ничья!");
+        }
+
+    };
+
+    public static Integer choose(String name) {
+        Random randomChoose = new Random();
+        int choose = randomChoose.nextInt(3);
+        if (choose == 0) {
+            System.out.println(name+" выкинул камень");
+        } else if (choose == 1) {
+            System.out.println(name+" выкинул ножницы");
+        } else {
+            System.out.println(name+" выкинул бумагу");
+        }
+        return choose;
     }
 }
